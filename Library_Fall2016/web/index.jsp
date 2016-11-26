6fecfd36cfdffd6e72256ba89f605fee47aa7a65
Skanda Narendra Bhargav <skandabhargav@gmail.com>
2016-11-26 11:17:43 -0800

    Check if show all works

---
+++ Library_Fall2016/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: SkandaBhargav
+  Date: 11/25/16
+  Time: 8:48 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
--- Working_Lab2_SpringMVC/src/main/java/edu/sjsu/cmpe275/lab2/controller/PhoneController.java
+++ Working_Lab2_SpringMVC/src/main/java/edu/sjsu/cmpe275/lab2/controller/PhoneController.java
@@ -1,35 +1,23 @@
 package edu.sjsu.cmpe275.lab2.controller;
 
-import java.util.List;
-
-import javax.persistence.Embeddable;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
-
-import edu.sjsu.cmpe275.lab2.entity.AddressEntity;
 import edu.sjsu.cmpe275.lab2.entity.PhoneEntity;
 import edu.sjsu.cmpe275.lab2.entity.UserEntity;
 import edu.sjsu.cmpe275.lab2.service.PhoneService;
 import edu.sjsu.cmpe275.lab2.service.UserSerivce;
-import edu.sjsu.cmpe275.lab2.validator.PhoneFormValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 
 /**
@@ -38,8 +26,8 @@ import edu.sjsu.cmpe275.lab2.validator.PhoneFormValidator;
  */
 @Controller
 public class PhoneController {
-	/*@Autowired
-	private	HttpServletResponse httpResponse;*/
+
+	private	HttpServletResponse httpResponse;
 
 	@Autowired
 	private PhoneService pService;
@@ -101,9 +89,9 @@ public class PhoneController {
 		return mv;
 	}
 
-	/*@RequestMapping(value = "/phone/{pid}", method = RequestMethod.GET)
+	@RequestMapping(value = "/phone/{pid}", method = RequestMethod.GET)
 	public Object showPhone(@PathVariable("pid") Integer id,
-			@RequestParam(value="json",required = false, defaultValue="false") String json) {
+							@RequestParam(value="json",required = false, defaultValue="false") String json) {
 		PhoneEntity pEntity = pService.findById(id);
 		List<UserEntity> users = uService.findAll();
 		List<UserEntity> ret = pService.retrieveUsers(id);
@@ -118,7 +106,7 @@ public class PhoneController {
 				mv.addObject(users);
 				mv.addObject(ret);
 				return mv;
-			} 
+			}
 			System.out.println(" **************** Returning the normal model view ****************");
 			ModelAndView mv = new ModelAndView("phones/phoneInfo");
 			httpResponse.setStatus(HttpStatus.OK.value());
@@ -138,7 +126,6 @@ public class PhoneController {
 		}
 
 	}
-*/
 	@RequestMapping(value = "/phone/{pid}", method = RequestMethod.DELETE)
 	public ModelAndView deleteUser(@PathVariable("pid") Integer id){
 		boolean status = pService.deleteById(id);
@@ -147,8 +134,8 @@ public class PhoneController {
 		if(status){
 			System.out.println("redirecting-------------");
 			ModelAndView modelAndView = new ModelAndView("phones/addPhone");
-			return modelAndView;	
-		}else{	
+			return modelAndView;
+		}else{
 			System.out.println("redirecting 233-------------");
 			ModelAndView modelAndView = new ModelAndView("phones/addPhone");
 			return modelAndView;
@@ -157,16 +144,16 @@ public class PhoneController {
 
 	@RequestMapping(value="/phone/{pid}", method = RequestMethod.POST)
 	public String updatePhone(@PathVariable("pid") Integer id,
-			@RequestParam String number,
-			@RequestParam String description,
-			@RequestParam String city,
-			@RequestParam String state,
-			@RequestParam String street,
-			@RequestParam String zip_code,
-			@RequestParam String uid,
-			ModelMap model,
-			HttpServletRequest  request,
-			HttpServletResponse  response ) {
+							  @RequestParam String number,
+							  @RequestParam String description,
+							  @RequestParam String city,
+							  @RequestParam String state,
+							  @RequestParam String street,
+							  @RequestParam String zip_code,
+							  @RequestParam String uid,
+							  ModelMap model,
+							  HttpServletRequest  request,
+							  HttpServletResponse  response ) {
 		System.out.println("Check the contents of returned user IDS "+uid);
 		PhoneEntity pEntity = pService.updatePhone((Integer)id,number,description,city,state,street,zip_code,uid);
 
@@ -174,7 +161,7 @@ public class PhoneController {
 		/*ModelAndView modelAndView = new ModelAndView("users/userInfo");
 		modelAndView.addObject("user", uEntity);
 		return modelAndView;*/
-		return "http://8.35.192.11//lab2-1.0/phone/"+pEntity.getId().toString();
+		return "http://8.35.192.11/lab2-1.0/phone/"+pEntity.getId().toString();
 
 	}
 
