adcd95fa5d34f70669c14ac29f57c02c05abd3b9
Skanda Narendra Bhargav <skandabhargav@gmail.com>
2016-11-26 13:14:21 -0800

    Added Security dependencies to the POM

---
+++ Working_Lab2_SpringMVC/src/main/java/edu/sjsu/cmpe275/lab2/security/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package edu.sjsu.cmpe275.lab2.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by SkandaBhargav on 11/26/16.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void ConfigureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("password").roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                .antMatchers("/phone/showall").access("hasRole('ROLE_ADMIN')")
+                .and().formLogin();
+    }
+}
---
+++ Working_Lab2_SpringMVC/src/main/java/edu/sjsu/cmpe275/lab2/security/SpringSecurityInitializer.java
@@ -0,0 +1,9 @@
+package edu.sjsu.cmpe275.lab2.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by SkandaBhargav on 11/26/16.
+ */
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
