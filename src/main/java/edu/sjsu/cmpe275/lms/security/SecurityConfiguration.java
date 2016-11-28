ab2e980626805858c5ca7c6e2e9951f20b50d5d9
Skanda Narendra Bhargav <skandabhargav@gmail.com>
2016-11-27 22:06:39 -0800

    Added JDBC based authentication for users and roles Need to create two new tables in Database for it to work.

--- src/main/java/edu/sjsu/cmpe275/lms/security/SecurityConfiguration.java
+++ src/main/java/edu/sjsu/cmpe275/lms/security/SecurityConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 /**
  * Created by SkandaBhargav on 11/26/16.
  */
@@ -14,6 +16,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    public void ConfigureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("select username,password, enabled from users where username=?")
+                .authoritiesByUsernameQuery("select username,role from user_roles where username=?");
+    }
+/*
     @Autowired
     public void ConfigureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("admin").password("password").roles("ADMIN");
@@ -26,5 +38,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and().formLogin();
         httpSecurity.csrf().disable();
     }
+*/
 
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                .antMatchers("/user/showall").access("hasRole('ROLE_ADMIN')")
+                .and().formLogin();
+        httpSecurity.csrf().disable();
+    }
 }
