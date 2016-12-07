70c1fb58c84e46fcc84ce677b2c0473fc8ec04ec
Skanda Narendra Bhargav <skandabhargav@gmail.com>
2016-12-07 12:55:09 -0800

    Merge remote-tracking branch 'remotes/origin/Transcation' into Librarian_Book_Delete
    # Conflicts:
    #	src/main/java/edu/sjsu/cmpe275/lms/controller/BookController.java
    #	src/main/java/edu/sjsu/cmpe275/lms/dao/BookDaoImpl.java
    #	src/main/java/edu/sjsu/cmpe275/lms/entity/Book.java

---
+++ src/main/java/edu/sjsu/cmpe275/lms/dao/UserBookCartDao.java
@@ -0,0 +1,29 @@
+package edu.sjsu.cmpe275.lms.dao;
+
+import edu.sjsu.cmpe275.lms.entity.UserBookCart;
+import edu.sjsu.cmpe275.lms.errors.Err;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+public interface UserBookCartDao {
+
+    /**
+     * Adds the given book against the user to card
+     *
+     * @param ubc UserBookCart
+     * @return true if add successful, false if failed
+     */
+    Err addUserBookToCart(UserBookCart ubc);
+
+    /**
+     * To get the cart for a user
+     *
+     * @param userid int userid
+     * @return List of UserBookCart
+     */
+    List<UserBookCart> getUserCart(int userid);
+
+    boolean removeCartEntry(UserBookCart ubc);
+}
---
+++ src/main/java/edu/sjsu/cmpe275/lms/dao/UserBookCartDaoImpl.java
@@ -0,0 +1,65 @@
+package edu.sjsu.cmpe275.lms.dao;
+
+import edu.sjsu.cmpe275.lms.entity.UserBookCart;
+import edu.sjsu.cmpe275.lms.errors.Err;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+import javax.persistence.Query;
+import java.util.List;
+
+@Transactional
+@Repository
+public class UserBookCartDaoImpl implements UserBookCartDao {
+
+    @PersistenceContext(type = PersistenceContextType.EXTENDED)
+    private EntityManager entityManager;
+
+    /**
+     * Adds the given book against the user to card
+     *
+     * @param ubc UserBookCart
+     * @return Err false if add successful, true with error message if failed
+     */
+    @Override
+    public Err addUserBookToCart(UserBookCart ubc) {
+        List<UserBookCart> ubcList = this.getUserCart(ubc.getUser_id());
+        if (ubcList.size() > 4) {
+            return new Err(true, "You can only add 5 books to the cart at a time");
+        }
+        for (UserBookCart u : ubcList) {
+            if (u.getBook_id() == ubc.getBook_id()) {
+                return new Err(true, "Book already added to cart!");
+            }
+        }
+        entityManager.persist(ubc);
+        return new Err();
+    }
+
+    /**
+     * To get the cart for a user
+     *
+     * @param userid int userid
+     * @return List of UserBookCart
+     */
+    @Override
+    public List<UserBookCart> getUserCart(int userid) {
+        String query = "Select ubc From UserBookCart ubc WHERE ubc.user_id = :userid";
+        Query q = entityManager.createQuery(query, UserBookCart.class);
+        q.setParameter("userid", userid);
+        return q.getResultList();
+    }
+
+    /**
+     * @param
+     */
+    @Override
+    public boolean removeCartEntry(UserBookCart ubc) {
+        entityManager.remove(ubc);
+        return true;
+    }
+
+}
