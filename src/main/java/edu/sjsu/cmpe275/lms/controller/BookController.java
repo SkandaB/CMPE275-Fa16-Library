1b9c73446870d8b16d40e79394a0472c42fec6ef
Skanda Narendra Bhargav <skandabhargav@gmail.com>
2016-12-18 18:50:17 -0800

    Change the return type of add book from Librarian dashboard Now it should redirect to dashboard after adding a book

--- src/main/java/edu/sjsu/cmpe275/lms/controller/BookController.java
+++ src/main/java/edu/sjsu/cmpe275/lms/controller/BookController.java
@@ -112,7 +112,7 @@ public class BookController {
     @Transactional
     @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping(value = "/addBook", method = RequestMethod.POST)
-    String addBookviaForm(@ModelAttribute("book") Book book, ModelAndView modelAndView, HttpServletRequest request, HttpServletResponse response) throws GeneralSecurityException, IOException, ServiceException {
+    ModelAndView addBookviaForm(@ModelAttribute("book") Book book, ModelAndView modelAndView, HttpServletRequest request, HttpServletResponse response) throws GeneralSecurityException, IOException, ServiceException {
         System.out.println("boook" + book);
         /**
          * Check if the mode of addition is via ISBN or advanced-mode.
@@ -137,8 +137,8 @@ public class BookController {
             book.setIsbn(book.getIsbn());
             addNewBook(book, book.getTitle(), book.getAuthor(), book.getYear_of_publication(), book.getPublisher(), response, user);
         }
-        return "redirect:/dashboard";
-        //return new ModelAndView("redirect:/register");
+        //return "librarian/dashboard";
+        return new ModelAndView("redirect:/lmsdashboard");
     }
 
     /**
